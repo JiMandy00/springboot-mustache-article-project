19f85b5fc93f89eae306b415cab0edf5a4a0f2e7
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-29 22:35:21 +0900

    2022 11 29 - 조회기능
    id를 통한 조회 기능을 작성 했습니다.

--- src/main/java/com/mustache/article/controller/ArticleController.java
+++ src/main/java/com/mustache/article/controller/ArticleController.java
@@ -1,23 +1,46 @@
 package com.mustache.article.controller;
 
 import com.mustache.article.dto.ArticleDto;
+import com.mustache.article.entity.Article;
+import com.mustache.article.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Optional;
+
+@Controller
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/new")
     public String createPage() {
-        return "new";
+        return "articles/new";
     }
 
     @PostMapping("")
     public String add(ArticleDto articleDto) {
         log.info(articleDto.getTitle());
         return ""; // 임시
+    }
+
+    @GetMapping("/{id}")
+    public String selectString(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
     }
 
 }
